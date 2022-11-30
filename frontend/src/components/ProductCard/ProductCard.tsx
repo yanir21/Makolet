90cda0ff4d874e2ebdd910959d3b99a466549d47
yanir21 <yanir334@gmail.com>
2022-11-30 18:51:36 +0200

    feat(ProductCard.tsx): card design

--- frontend/src/components/ProductCard/ProductCard.tsx
+++ frontend/src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import "./productCard.css";
 import { Product } from "../../models/Product";
+import Button from "react-bootstrap/Button";
 
 const ProductCard = (product: Product) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={product.imageUrl} />
+    <Card className="card">
+      <Card.Img className="image" variant="top" src={product.imageUrl} />
       <Card.Body>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{product.description}</Card.Text>
       </Card.Body>
       <Card.Body>
-        <Card.Link href="#">+</Card.Link>
-        <Card.Link href="#">-</Card.Link>
+        <Card.Footer>
+          <div className="button-section">
+            <Button className="action-button">+</Button>
+            <Button variant="danger" className="action-button">
+              -
+            </Button>
+          </div>
+        </Card.Footer>
       </Card.Body>
     </Card>
   );
--- frontend/src/components/Views/HomePage/homePage.tsx
+++ frontend/src/components/Views/HomePage/homePage.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Product } from "../../../models/Product";
 import { getAllProducts } from "../../../services/cartService";
-import ProductCard from "../../ProductCard/ProductCard";
+import ProductCard from "../../ProductCard/productCard";
+import "./homepage.css";
 
 const Home = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -16,11 +17,15 @@ const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <div className="title"></div>
+        <div className="title">All Products</div>
         <div className="product-list">
-          {products.map((product) => (
-            <ProductCard {...product} />
-          ))}
+          <div className="row">
+            {products.map((product) => (
+              <div className="col-4">
+                <ProductCard key={product.name} {...product} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
